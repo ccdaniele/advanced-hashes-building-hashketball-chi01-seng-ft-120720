6973b385ffdaf19c7d3099d64ac994629294754e
Daniel Calderon <danieleduardocald@gmail.com>
2020-12-04 14:32:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,18 +2,18 @@ require'pry'
 def game_hash
   
   {:home => {
-    :team_name => "team name"
-    :colors => "team color"
-    :players => [
+    :team_name => "team name",
+    :colors => "team color",
+    :players =>[
       {
-        :player_name 
-        :number 
-        :shoe
-        :points
-        :rebounds
-        :assists
-        :steals
-        :blocks
+        :player_name,
+        :number, 
+        :shoe,
+        :points,
+        :rebounds,
+        :assists,
+        :steals,
+        :blocks,
         :slam_dunks
         
       }
